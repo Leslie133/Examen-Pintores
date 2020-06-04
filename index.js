4f72be58c5e525944a4161fc90655cbdb7c4ce5e
Leslie133 <64936473+Leslie133@users.noreply.github.com>
2020-06-03 23:41:17 -0600

    Leslie_Márquez
    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+"name":"informe",
+    "version":"1.0.0",
+    "description":"",
+    "main":"index.js",
+    "scripts":{
+        "test":"echo\"Error: no test specified\"&& exit 1"
+    },
+    "author": "",
+    "licence":"ISC",
+    "dependencies":{
+        "express": "^4.1.1",
+        "hbs": "^4.1.1",
+        "mongoose": "^5.9.16"
+    }
+}
